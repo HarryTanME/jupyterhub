63a81c9703c9192ead24a582ad6685024ef5a297
EC2 Default User <ec2-user@ip-10-0-1-10.us-west-2.compute.internal>
2017-12-24 17:31:27 +0000

    added stats calc.

--- share/jupyter/hub/static/js/tutorial.js
+++ share/jupyter/hub/static/js/tutorial.js
@@ -14,20 +14,27 @@ require(["jquery", "jhapi"], function ($, JHAPI) {
         var github = $(this).data('github');
         var image = $(this).data('image');
         var session_name =$(this).data('name');
+        var default_notebook = $(this).data('path');
         
         if(image == 'dl'){
-            image = "wodeai/deeplearning:hubdev"
+            image = "wodeai/deeplearning:hubdev";
         }else if(image == 'nlp'){
-            image = "wodeai/nlp:hubdev"
+            image = "wodeai/nlp:hubdev";
         } else if(image == 'r'){
-            image = "wodeai/r-notebook:hubdev"
+            image = "wodeai/r-notebook:hubdev";
         }else{
-            image = "wodeai/datascience:hubdev"
+            image = "wodeai/datascience:hubdev";
         }
-        var datastr='{"docker_image": "'.concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat('; "}')
-        
+        if (typeof default_notebook == 'undefined'){
+            var datastr='{"docker_image": "'.concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat(';" }');
+        }else{
+            var datastr='{"docker_image": "'.concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat('; ", ').concat(' "argv":["--NotebookApp.default_url=').concat(default_notebook).concat('"] }');
+        }
+        $('body').append(datastr)
+                     
         api.start_named_session(user, session_name, {
             success: function (response) {
+                btn.removeAttr('disabled');
                 var data = JSON.parse(response);
                 //window.location.replace(data.url);
                 window.open(data.url);
