dbd853f84d4c8f99867b4cf90708823432d3b6c4
EC2 Default User <ec2-user@ip-10-0-1-10.us-west-2.compute.internal>
2018-01-04 17:49:37 +0000

    make tutorials runable

--- share/jupyter/hub/static/js/tutorial.js
+++ share/jupyter/hub/static/js/tutorial.js
@@ -15,20 +15,26 @@ require(["jquery", "jhapi"], function ($, JHAPI) {
         var image = $(this).data('image');
         var session_name =$(this).data('name');
         var default_notebook = $(this).data('path');
+        var sources="";
         
         if(image == 'dl'){
             image = "wodeai/deeplearning:hubdev";
+            sources='"data_sources":[{"source":"/data/modelzoo" ,"target":"/home/wode-user/shared/modelzoo", "control":"ro"}],'
         }else if(image == 'nlp'){
             image = "wodeai/nlp:hubdev";
+            sources='"data_sources":[{"source":"/data/data" ,"target":"/home/wode-user/shared/data", "control":"ro"}, {"source":"/data/modelzoo" ,"target":"/home/wode-user/shared/modelzoo", "control":"ro"}],'
         } else if(image == 'r'){
             image = "wodeai/r-notebook:hubdev";
         }else{
             image = "wodeai/datascience:hubdev";
         }
+        
+
+        
         if (typeof default_notebook == 'undefined'){
-            var datastr='{"docker_image": "'.concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat(';" }');
+            var datastr='{'.concat(sources).concat('"docker_image": "').concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat(';" }');
         }else{
-            var datastr='{"docker_image": "'.concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat('; ", ').concat(' "argv":["--NotebookApp.default_url=').concat(default_notebook).concat('"] }');
+            var datastr='{'.concat(sources).concat('"docker_image": "').concat(image).concat('", "pre-cmd":"git clone ').concat(github).concat('; ", ').concat(' "argv":["--NotebookApp.default_url=').concat(default_notebook).concat('"] }');
         }
         api.start_named_session(user, session_name, {
             success: function (response) {
